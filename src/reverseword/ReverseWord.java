6835747392967eaf2590f9bfff3fd6e3c18b1849
vasamsheridan <vasam@sheridancollege.ca>
2023-05-25 14:31:57 -0700

    version 1

---
+++ src/reverseword/ReverseWord.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package reverseword;
+
+/**
+ *
+ * @author vasam
+ */
+import java.util.*;
+public class ReverseWord {
+
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter name");
+        String name = scan.next();
+        System.out.println(name);
+        char[] reverse = new char[name.length()];
+        for (int i = 0; i<name.length(); i++) {
+            reverse[i] = name.charAt(i);
+        }
+        for (int i = reverse.length-1;i>=0;i--){
+            System.out.println(reverse[i]);
+        }
+        
+    }
+    
+}
